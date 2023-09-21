e17912e327e3edfd4a94750924f82566eb4ddba1
Vladimir Kostyuk <voldemar22864@gmail.com>
2023-09-21 12:21:31 +0300

    fixed: bool func

--- CheckGameState.cpp
+++ CheckGameState.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 
-char CheckGameState(char (&gameField)[3][3]) {
+bool CheckGameState(char (&gameField)[3][3]) {
     for (int i = 0; i < 3; ++i) {
-        if (gameField[i][0] == 'O' && gameField[i][1] == 'O' && gameField[i][2] == 'O' ||
-            gameField[i][0] == 'X' && gameField[i][1] == 'X' && gameField[i][2] == 'X') {
-            return '0';
+        if (gameField[i][0] == 'O' && gameField[i][1] == 'O' && gameField[i][2] == 'O') {
+            return false;
         }
     }
     for (int j = 0; j < 3; ++j) {
-        if (gameField[0][j] == 'O' && gameField[1][j] == 'O' && gameField[2][j] == 'O' ||
-            gameField[0][j] == 'X' && gameField[1][j] == 'X' && gameField[2][j] == 'X') {
-            return '0';
+        if (gameField[0][j] == 'O' && gameField[1][j] == 'O' && gameField[2][j] == 'O') {
+            return false;
         }
     }
 
     if (gameField[0][0] == 'O' && gameField[1][1] == 'O' && gameField[2][2] == 'O' ||
-        gameField[0][0] == 'X' && gameField[1][1] == 'X' && gameField[2][2] == 'X' ||
-        gameField[0][2] == 'O' && gameField[1][1] == 'O' && gameField[2][0] == 'O' ||
-        gameField[0][2] == 'X' && gameField[1][1] == 'X' && gameField[2][0] == 'X') {
-        return '0';
+        gameField[0][2] == 'O' && gameField[1][1] == 'O' && gameField[2][0] == 'O') {
+        return false;
     }
 
     int check = 0;
@@ -31,12 +27,8 @@ char CheckGameState(char (&gameField)[3][3]) {
     }
 
     if (check > 0) {
-        return '1';
+        return true;
     } else {
-        return '0';
+        return false;
     }
-}
-
-int main() {
-
 }
