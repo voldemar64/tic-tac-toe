d4d12a4c4ee4569eb961c2ca2938e6835734f664
Vladimir Kostyuk <voldemar22864@gmail.com>
2023-09-21 12:56:50 +0300

    fixed: winner shoutout added: main

--- main.cpp
+++ main.cpp
@@ -48,17 +48,20 @@ void PlayerTurn(char (&gameField)[3][3]){
 bool CheckGameState(char (&gameField)[3][3]) {
     for (int i = 0; i < 3; ++i) {
         if (gameField[i][0] == 'O' && gameField[i][1] == 'O' && gameField[i][2] == 'O') {
+            std:: cout << "bro u trash. Delete internet and do not enter it anymore!";
             return false;
         }
     }
     for (int j = 0; j < 3; ++j) {
         if (gameField[0][j] == 'O' && gameField[1][j] == 'O' && gameField[2][j] == 'O') {
+            std:: cout << "bro u trash. Delete internet and do not enter it anymore!";
             return false;
         }
     }
 
     if (gameField[0][0] == 'O' && gameField[1][1] == 'O' && gameField[2][2] == 'O' ||
         gameField[0][2] == 'O' && gameField[1][1] == 'O' && gameField[2][0] == 'O') {
+        std:: cout << "bro u trash. Delete internet and do not enter it anymore!";
         return false;
     }
 
@@ -74,10 +77,21 @@ bool CheckGameState(char (&gameField)[3][3]) {
     if (check > 0) {
         return true;
     } else {
+        std:: cout << "I tricked you. Nobody won. Ha-ha-ha!";
         return false;
     }
 }
 
 int main() {
-    
+    char gameField[3][3] {{'_', '_', '_'},
+                          {'_', '_', '_'},
+                          {'_', '_', '_'},
+                          };
+    while (CheckGameState(gameField)) {
+        PlayerTurn(gameField);
+        if (CheckGameState((gameField))) {
+            AITurn(gameField);
+        }
+        PrintGameField(gameField);
+    }
 }
